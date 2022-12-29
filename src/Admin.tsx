7fa8439222d9aa338a8235734fa81d52951fe1d3
IgboPharaoh <solomoneze62@gmail.com>
2022-12-29 22:21:18 +0100

    implemented filter and sort feature

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,37 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header, FederationCard } from './components';
 import { Federation } from './federation.types';
 import { data } from './federation.data';
-
+import { Stack } from '@chakra-ui/react';
 
 export const Admin = React.memo(() => {
-  return (
-    <div className="App">
-      <main style={wrapperStyles}>
-        <Header />
-        <section style={federationsListStyles}>
-          {
-            data.federations.map((federation: Federation) => {
-              return <FederationCard
-                        key={federation.mint_pubkey}
-                        federation={federation}
-                        onClick={() => console.log("clicked")}
-                      />
-            })
-          }
-        </section>
-      </main>
-    </div>
-  );
+    const [fedlist, setFedlist] = useState<Federation[]>(data.federations);
+
+    const filterFederations = (filter: undefined | boolean) => {
+        let federations = filter === undefined ? data.federations : data.federations.filter((federation) => federation.details.active === filter);
+        setFedlist(federations);
+    };
+
+    enum Sort {
+        Ascending,
+        Descending,
+        Date,
+    }
+
+    const sortFederations = (sort: Sort) => {
+        const fedListCopy = [...fedlist];
+
+        switch (sort) {
+            case Sort.Ascending: {
+                const result = fedListCopy.sort((a, b) => (a.details.name < b.details.name ? -1 : a.details.name > b.details.name ? 1 : 0));
+
+                return setFedlist(result);
+            }
+
+            case Sort.Descending: {
+                const result = fedListCopy.sort((a, b) => (a.details.name < b.details.name ? 1 : a.details.name > b.details.name ? -1 : 0));
+
+                return setFedlist(result);
+            }
+
+            case Sort.Date: {
+                const result = fedListCopy.sort((a, b) =>
+                    a.details.date_created < b.details.date_created ? 1 : a.details.date_created > b.details.date_created ? -1 : 0
+                );
+
+                return setFedlist(result);
+            }
+
+            default: {
+                return setFedlist(data.federations);
+            }
+        }
+    };
+
+    return (
+        <div className='App'>
+            <div style={wrapperStyles}>
+                <Header
+                    data={data.federations}
+                    ascending={() => {
+                        sortFederations(Sort.Ascending);
+                    }}
+                    descending={() => {
+                        sortFederations(Sort.Descending);
+                    }}
+                    datecCreated={() => {
+                        sortFederations(Sort.Date);
+                    }}
+                    isActive={() => {
+                        filterFederations(true);
+                    }}
+                    isArchived={() => {
+                        filterFederations(false);
+                    }}
+                    isAll={() => {
+                        filterFederations(undefined);
+                    }}
+                />
+                <Stack spacing={6} marginTop={6}>
+                    {fedlist.map((federation: Federation) => {
+                        return <FederationCard key={federation.mint_pubkey} federation={federation} onClick={() => console.log('clicked')} />;
+                    })}
+                </Stack>
+            </div>
+        </div>
+    );
 });
 
 const wrapperStyles = {
-  margin: 32,
-}
-
-const federationsListStyles = {
-  paddingTop: 24,
-  display: "flex",
-  flexDirection: "column" as "column",
-  gap: 24,
-}
+    margin: 32,
+};
