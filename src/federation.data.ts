c6044331e5d5d8e60a54b6d982a072a7f50a449b
IgboPharaoh <solomoneze62@gmail.com>
2022-12-31 09:09:47 +0100

    update dummy data

--- src/federation.data.ts
+++ src/federation.data.ts
@@ -1,41 +1,41 @@
 // Mock information
 export const data = {
-    "federations": [
+    federations: [
         {
-            "federation_id": "Hals_trusty_mint",
-            "mint_pubkey": "4222d0e6f9f4e0a9d471c81b3ee454032cddc8590a9aebf43332bb2d3ecb976c",
-            "description": "Hals Trusty Mint",
-            "date_created": "2018-01-01T00:00:00Z",
-            "details": {
-            "name": "Hals_trusty_mint",
-            "description": "Hals_trusty_mint",
-            "url": ""
-            }
+            federation_id: 'Hals_trusty_mint',
+            mint_pubkey: '4222d0e6f9f4e0a9d471c81b3ee454032cddc8590a9aebf43332bb2d3ecb976c',
+            details: {
+                description: 'Hals Trusty Mint',
+                date_created: '2018-01-01T00:00:00Z',
+                name: 'Hals_trusty_mint',
+                url: '',
+                active: true,
+            },
         },
 
         {
-            "federation_id": "FTX_yield_gen",
-            "mint_pubkey": "c679bce3d2bb23334fbea9a0958cddc230454ee3b18c174d9a0e4f9f6e0d2224",
-            "description": "FTX yield generator",
-            "date_created": "2018-01-01T00:00:00Z",
-            "details": {
-            "name": "FTX_yield_gen",
-            "description": "FTX yield generator",
-            "url": ""
-            }
+            federation_id: 'FTX_yield_gen',
+            mint_pubkey: 'c679bce3d2bb23334fbea9a0958cddc230454ee3b18c174d9a0e4f9f6e0d2224',
+            details: {
+                description: 'FTX yield generator',
+                date_created: '2019-01-01T00:00:00Z',
+                name: 'FTX_yield_gen',
+                url: '',
+                active: true,
+            },
         },
 
         {
-            "federation_id": "Qala_devs",
-            "mint_pubkey": "1d2eb31bb4c307b466e1a7c910693bc5754799ec705b14f760e74d5d4968acf9",
-            "description": "Qala_devs stacking sats",
-            "date_created": "2018-01-01T00:00:00Z",
-            "details": {
-            "name": "Qala_devs",
-            "description": "Qala_devs",
-            "url": ""
-            }
+            federation_id: 'Qala_devs',
+            mint_pubkey: '1d2eb31bb4c307b466e1a7c910693bc5754799ec705b14f760e74d5d4968acf9',
+            details: {
+                description: 'Qala_devs stacking sats',
+                date_created: '2020-01-01T00:00:00Z',
+                name: 'Qala_devs',
+                url: '',
+                active: false,
+            },
         },
-        ],
-        "version_hash": "f876d12a78df7d4ef5b3a4913590ded6bfe5c3b1"
-    }
+    ],
+    version_hash: 'f876d12a78df7d4ef5b3a4913590ded6bfe5c3b1',
+};
