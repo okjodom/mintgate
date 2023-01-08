6f4a95dd22305d1859330cbaab89a76114a331dc
IgboPharaoh <solomoneze62@gmail.com>
2023-01-08 23:29:09 +0100

    wrapped app chakraprovider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Admin } from './Admin';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <Admin />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ChakraProvider>
+            <Admin />
+        </ChakraProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
