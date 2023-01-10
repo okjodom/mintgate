8c912108376dfe773a553414da9a075385ae57f3
okjodom <okjodom@gmail.com>
2023-01-09 19:02:16 -0800

    showcase pending transaction

--- src/components/DepositTab.tsx
+++ src/components/DepositTab.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Tab, TabPanel, Stack, Card, CardBody, CardFooter, Image, Heading, Text, Spacer, Flex } from '@chakra-ui/react'
+import { Tab, TabPanel, Stack, Card, CardBody, CardFooter, Image, Heading, Text, Spacer, Flex, Link, Progress } from '@chakra-ui/react'
 import { Button } from '.';
 
 
@@ -43,8 +43,29 @@ export const DepositTab = React.memo((_: DepositTabProps): JSX.Element => {
                     }
                 }
             case DepositStatus.Pending:
+                let pendingTxProps: ShowTransactionProps = {
+                    txid: 'de3d5bf1e3c1b3be2a1e025825f751629390ad60c8f91723e330f2356d99c59b',
+                    address: 'bc1qgf60crqtlxn7279tgh8lsxzagmu97cyuwtykxwv026s9hwg427fsjvw7uz',
+                    amount_btc: 0.00013813,
+                    confirmations: 1,
+                    confirmationsRequired: 3,
+                }
                 return {
-                    content: <ShowDepositAddress />,
+                    content: <ShowTransaction {...pendingTxProps} />,
+                    actions: [
+                        {
+                            label: 'Copy Transaction ID',
+                            onClick: () => setDepositStatus(DepositStatus.Complete)
+                        },
+                        {
+                            label: 'Complete',
+                            onClick: () => setDepositStatus(DepositStatus.Complete)
+                        },
+                    ],
+                    infographic: {
+                        imgUrl: 'https://www.maxpixel.net/static/photo/1x/Bitcoin-Logo-Bitcoin-Icon-Bitcoin-Btc-Icon-Currency-6219384.png',
+                        altText: 'Pending bitcoin transaction'
+                    }
                 }
             case DepositStatus.Complete:
                 return {
@@ -93,14 +114,15 @@ export const DepositCard = React.memo((props: DepositCardProps): JSX.Element => 
                 })}
             </CardFooter>
         </Stack>
-        <Spacer />
-        {infographic && <Image
-
-            objectFit='cover'
-            maxW={{ base: '100%', sm: '200px' }}
-            src={infographic.imgUrl}
-            alt={infographic.altText}
-        />}
+        {infographic && <>
+            <Spacer />
+            <Image
+                objectFit='cover'
+                maxW={{ base: '100%', sm: '200px' }}
+                src={infographic.imgUrl}
+                alt={infographic.altText}
+            />
+        </>}
     </Card>
 });
 
@@ -125,3 +147,40 @@ const ShowDepositAddress = (): JSX.Element => {
         </Flex>
     </>
 }
+
+interface ShowTransactionProps {
+    txid: string,
+    address: string,
+    amount_btc: number,
+    confirmations: number,
+    confirmationsRequired: number,
+}
+
+const ShowTransaction = (props: ShowTransactionProps) => {
+    const { txid, address, amount_btc, confirmations, confirmationsRequired } = props;
+
+    const computeProgress = () => {
+        return 100 * confirmations / confirmationsRequired;
+    }
+
+    return (
+        <>
+            <Heading size="md">
+                Bitcoin Deposit
+            </Heading>
+            <Flex align="center" mt={3}>
+                <Text fontSize="lg" mr={2}>Amount:</Text>
+                <Text fontSize="lg" fontWeight="bold">{amount_btc} BTC</Text>
+            </Flex>
+            <Text mt={4}>
+                Confirmations: {confirmations} / {confirmationsRequired} required
+            </Text>
+            <Progress value={computeProgress()} size='xs' colorScheme='orange' hasStripe />
+            <Text mt={2}>
+                Sent to address: {address}
+            </Text>
+            <Text mt={2}>
+                Transaction id: <Link href={`https://mempool.space/tx/${txid}`}>{txid}</Link>
+            </Text></>
+    );
+};
