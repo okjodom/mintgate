26dd05e6adfb5b5fdf287d147802605b4b8cebb8
okjodom <okjodom@gmail.com>
2023-01-10 11:10:09 -0800

    introduce custom tab headers

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,6 @@ export { FederationCard } from './FederationCard';
 export { Header } from './Header'
 export { Input } from './Input';
 export { ConnectFederation } from './ConnectFederation';
+export { TabHeader } from './TabHeader';
 export { InfoTab, InfoTabHeader } from './InfoTab';
 export { DepositTab, DepositTabHeader } from './DepositTab';
