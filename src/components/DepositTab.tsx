487194adcef492fd9c3eaf3c53a47a2fa4924677
okjodom <okjodom@gmail.com>
2023-01-09 16:24:07 -0800

    showcase entry into deposit experience

--- src/components/DepositTab.tsx
+++ src/components/DepositTab.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Tab, TabPanel, Stack } from '@chakra-ui/react'
-import { Input, Button } from '.';
+import { Tab, TabPanel, Stack, Card, CardBody, CardFooter, Image, Heading, Text, Spacer } from '@chakra-ui/react'
+import { Button } from '.';
 
 
 export const DepositTabHeader = (): JSX.Element => {
@@ -12,16 +12,32 @@ interface DepositTabProps {
 
 export const DepositTab = React.memo((_: DepositTabProps): JSX.Element => {
     return <TabPanel>
-        <Stack spacing='4' maxWidth='md'>
-            <Input
-                labelName='Federation address:'
-                placeHolder='Enter wallet address'
-                value='5766898990ddjjdhdh88747'
-                onChange={() => {
-                    console.log('');
-                }}
+        <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow='hidden'
+            variant='unstyled'
+        >
+            <Stack>
+                <CardBody>
+                    <Heading size='md'>Federation deposit address</Heading>
+
+                    <Text py='3'>
+                        This bitcoin address allows you <br/> to deposit funds into this federation
+                    </Text>
+                </CardBody>
+
+                <CardFooter gap='2'>
+                    <Button label='Share' onClick={() => { }} />
+                    <Button label='Copy' onClick={() => { }} />
+                </CardFooter>
+            </Stack>
+            <Spacer />
+            <Image
+                objectFit='cover'
+                maxW={{ base: '100%', sm: '200px' }}
+                src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png'
+                alt='Bitcoin Address QR Code'
             />
-            <Button label='Deposit' borderRadius='4' onClick={() => console.log('clicked')} />
-        </Stack>
-    </TabPanel>
+        </Card>
+    </TabPanel >
 });
